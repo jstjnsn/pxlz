0020a40dcb3ae8c38e440c1362b13a741dc7385c
Joost Jansen <hello@joostjansen.me>
2023-12-02 20:14:40 +0100

    working pixel grid (sortof)

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ import { WebSocketServer } from "ws";
 
 const server = new WebSocketServer({ port: 8080 });
 
+const boxes = Array.from({ length: 10000 }, () =>
+  Math.floor(Math.random() * 100000)
+);
+
+let messageCount = 0;
+
 server.on("connection", (connection) => {
   console.log("New connection");
+  connection.send(JSON.stringify({ boxes }));
   connection.on("message", (data) => {
     const deserialisedData = JSON.parse(data);
-    console.log(deserialisedData);
+    console.log(messageCount, deserialisedData);
+    messageCount++;
     for (const client of server.clients) {
       const serialisedData = JSON.stringify(deserialisedData);
       client.send(serialisedData);
