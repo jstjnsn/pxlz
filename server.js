496d014f55adf6a696cc90d4b49ba2a383be6bc3
Joost Jansen <hello@joostjansen.me>
2023-12-03 13:59:20 +0100

    add static folder

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { WebSocketServer } from "ws";
 
-const server = new WebSocketServer({ port: 8080 });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const boxes = Array.from({ length: 10000 }, () =>
-  Math.floor(Math.random() * 100000)
+// EXPRESS SERVER
+
+const expressServer = express();
+
+expressServer.use(express.static("public"));
+
+expressServer.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "app.html"));
+});
+
+expressServer.listen(3000);
+
+// WEBSOCKET SERVER
+
+const websocketServer = new WebSocketServer({ port: 8080 });
+
+const boxes = Array.from(
+  new Set(
+    Array.from({ length: 15000 }, () => Math.floor(Math.random() * 100000))
+  ).entries()
 );
 
 let messageCount = 0;
 
-server.on("connection", (connection) => {
-  console.log("New connection");
+websocketServer.on("connection", (connection) => {
   connection.send(JSON.stringify({ boxes }));
   connection.on("message", (data) => {
     const deserialisedData = JSON.parse(data);
-    console.log(messageCount, deserialisedData);
     messageCount++;
-    for (const client of server.clients) {
+    for (const client of websocketServer.clients) {
       const serialisedData = JSON.stringify(deserialisedData);
       client.send(serialisedData);
     }
